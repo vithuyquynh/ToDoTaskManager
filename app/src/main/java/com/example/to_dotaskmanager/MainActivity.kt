fc5a5bad26989adda6755554b7d16ca8dfe80ca1
vophiho <voho39851@gmail.com>
2023-07-19 19:10:07 +0700

    ToDoTaskManager

---
+++ app/src/main/java/com/example/to_dotaskmanager/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.to_dotaskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.to_dotaskmanager.ui.theme.ToDoTaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoTaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ToDoApp()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun ToDoApp(modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(
+                top = 24.dp,
+                bottom = 8.dp,
+            )
+        )
+        Text(
+            text = "Nice Work!",
+            fontSize = 16.sp,
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun ToDoTaskManagerPreview() {
+    ToDoTaskManagerTheme {
+        ToDoApp()
+    }
+}
